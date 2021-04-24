79a4f5b5f2cf39ae9631c2422986799a621f5fda
Alexandre Cuva <alexandre.cuva@gmail.com>
2021-04-24 13:13:02 +0200

    Default Changelist

--- src/test/java/org/example/kata/quarkus/features/CarTaxShould.java
+++ src/test/java/org/example/kata/quarkus/features/CarTaxShould.java
@@ -7,6 +7,8 @@ import org.example.kata.quarkus.domain.vehicules.Car;
 import org.example.kata.quarkus.domain.vehicules.Van;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import javax.ws.rs.core.MediaType;
 
@@ -55,14 +57,16 @@ public class CarTaxShould {
                 .body("tax", equalTo(expected));
     }
 
-    @DisplayName("Return 65 GBP When car register after or equal 1st march 2001 and co2 up to 100")
-    @Test
-    public void return_65GBP_when_register_after_equal_20010301_and_co2_up_to_100(){
-        int expected = 65;
+    @DisplayName("Return expected GBP When car register after or equal 1st march 2001 and co2")
+    @ParameterizedTest
+    @CsvSource({
+        "0,65"
+    })
+    public void return_65GBP_when_register_after_equal_20010301_and_co2_up_to_100(int co2, int expected){
         given()
                 .body(new Car(
                                 LocalDateTime.of(2001, 1, 1, 0, 0),
-                                0, 100)
+                                0, co2)
                         , ObjectMapperType.JACKSON_2)
                 .header("Content-Type", MediaType.APPLICATION_JSON)
                 .when()
